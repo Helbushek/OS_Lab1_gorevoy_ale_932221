c0edb0aba2b3f4edea04c820c5d7280001710802
Helbushek <gorevaya134@gmail.com>
2025-11-09 11:41:41 +0700

    added consumer and provider stub threads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
+
+#include "some_class.h"
+
+MyClass someData;
+
+std::mutex mtx; 
+std::condition_variable cv;
+std::atomic<bool> isFinishing = false;
+
+void provider()
+{
+    std::lock_guard<decltype(mtx)> lock(mtx);
+    
+    while(!isFinishing)
+    {
+        std::cout << "Provider is waiting\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    cv.notify_all();
+}
+
+void consumer()
+{
+    std::unique_lock<std::mutex> lock(mtx);
+    cv.wait(lock, []()
+    {
+        return !isFinishing;
+    });
+
+    std::cout << "Consumer stopping\n";
+}
 
 int main()
 {
-    std::cout << "Hello, Ubuntu 24!" << std::endl;
+    std::thread providerThread(provider);
+    std::thread consumerThread(consumer);
+
+    providerThread.join();
+    consumerThread.join();
+
+    std::cout << "Execution completed\n";
+    return EXIT_SUCCESS;
 }
