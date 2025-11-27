90cf09683eb912e09f34d6741cafafc555e9e378
Helbushek <gorevaya134@gmail.com>
2025-11-27 15:16:19 +0700

    lab2 export

--- main.cpp
+++ main.cpp
@@ -1,83 +1,161 @@
-#include <iostream>
-#include <thread>
-#include <chrono>
-#include <mutex>
-#include <atomic>
-#include <condition_variable>
-#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
 
-#include "some_class.h"
+constexpr int PORT = 8009;
 
-MyClass* someData = new MyClass();
+volatile sig_atomic_t wasSigHup = 0;
+int listen_fd = -1;
+int client_fd = -1;
 
-std::mutex mtx; 
-std::condition_variable cv;
-std::atomic<bool> isFinishing = false;
-std::atomic<bool> isProvided = false;
-
-void signalHandler(int signal)
+void sigHupHandler(int) 
 {
-    std::cout << "\b\bEXIT SIGNAL CAUGHT\n";
-    isFinishing.store(true);
-    cv.notify_all();
+    wasSigHup = 1;
 }
 
-void provider()
+int main() 
 {
-    while (!isFinishing.load()) ///< Check for exit flag
-    {
-        std::unique_lock<decltype(mtx)> lock(mtx); ///< Locking our mutex
+    struct sigaction sa;
+    sigaction(SIGHUP, nullptr, &sa);
+    sa.sa_handler = sigHupHandler;
+    sa.sa_flags |= SA_RESTART;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGHUP, &sa, nullptr);
 
-        std::cout << "[PROVIDER] Is providing...\n"; 
-        someData->Count(); ///< Doing some job
-        isProvided.store(true); ///< Setting flag that the job is completed
-        cv.notify_one(); ///< Here we say to consumer that "Hey, you can receive my work result, i`ve finished"
-        cv.wait_for(lock, std::chrono::seconds(1), []()
-        {
-            return isFinishing.load();
-        }); ///< Here we wait for one second, simulating work with ability to break the thread`s sleep with confitional variable
+    sigset_t blockedMask, origMask;
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGHUP);
+    if (sigprocmask(SIG_BLOCK, &blockedMask, &origMask) == -1) 
+    {
+        perror("sigprocmask");
+        return EXIT_FAILURE;
     }
 
-    std::cout << "[PROVIDER] Stopping...\n"; ///< We will come here only if isFinished
-}
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_fd == -1) 
+    { 
+        perror("socket"); 
+        return EXIT_FAILURE; 
+    }
 
-void consumer(int id)
-{
-    while (!isFinishing.load()) ///< Same, just work until user says not to
+    int opt = 1;
+    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in serv_addr{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(PORT);
+
+    if (bind(listen_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) 
     {
-        std::unique_lock<decltype(mtx)> lock(mtx); ///< Here we receive out locked mutex
-        cv.wait(lock, [&]()
-        {
-            return isProvided.load() || isFinishing.load();
-        }); ///< The most interesting part - here we free the thread until we get cv notication AND lambda returns true
+        perror("bind");
+        close(listen_fd);
+        return EXIT_FAILURE;
+    }
 
-        if (isFinishing.load())
+    if (listen(listen_fd, SOMAXCONN) == -1) 
+    {
+        perror("listen");
+        close(listen_fd);
+        return EXIT_FAILURE;
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (true) 
+    {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(listen_fd, &fds);
+        int max_fd = listen_fd;
+
+        if (client_fd != -1) 
         {
-            break; ///< Check to exit if we get notification to exit and not to receive processed data
+            FD_SET(client_fd, &fds);
+            if (client_fd > max_fd) max_fd = client_fd;
         }
 
-        std::cout << "[CONSUMER" + std::to_string(id) + "] Fetched provider`s signal: " + someData->GetString() + std::to_string(someData->GetCounter()) + "\n"; ///< Just logging our id and data
-        isProvided.store(false);
+        int ret = pselect(max_fd + 1, &fds, nullptr, nullptr, nullptr, &origMask);
+        if (ret == -1) 
+        {
+            if (errno == EINTR) 
+            {
+                if (wasSigHup) 
+                {
+                    printf("Received SIGHUP\n");
+                    wasSigHup = 0;
+                }
+                continue;
+            }
+            perror("pselect");
+            break;
+        }
+
+        if (FD_ISSET(listen_fd, &fds)) 
+        {
+            struct sockaddr_in cli{};
+            socklen_t len = sizeof(cli);
+            int fd = accept(listen_fd, (struct sockaddr*)&cli, &len);
+            if (fd == -1) 
+            {
+                perror("accept");
+                continue;
+            }
+
+            char ip[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &cli.sin_addr, ip, sizeof(ip));
+            printf("New connection from %s:%d\n", ip, ntohs(cli.sin_port));
+
+            if (client_fd == -1) 
+            {
+                client_fd = fd;
+                printf("Connection accepted\n");
+            } 
+            else 
+            {
+                printf("Rejecting additional connection from %s:%d\n", ip, ntohs(cli.sin_port));
+                close(fd);
+            }
+        }
+
+        if (client_fd != -1 && FD_ISSET(client_fd, &fds)) 
+        {
+            char buf[4096];
+            ssize_t n = recv(client_fd, buf, sizeof(buf), 0);
+
+            if (n > 0) 
+            {
+                printf("Received %zd bytes\n", n);
+            } 
+            else if (n == 0) 
+            {
+                printf("Client closed connection\n");
+                close(client_fd);
+                client_fd = -1;
+            } 
+            else 
+            {
+                perror("recv");
+                close(client_fd);
+                client_fd = -1;
+            }
+        }
     }
 
-    std::cout << "[CONSUMER" + std::to_string(id) + "] Stopping...\n"; ///< Logging that we are stopping
-}
-
-int main()
-{
-    signal(SIGTERM, signalHandler); ///< Reusing system signals to call our function of setting exit flag
-    signal(SIGINT, signalHandler);
-
-    std::thread providerThread(provider); ///< Starting provider thread
-
-    std::thread consumerThread1(consumer, 1); ///< Starting consumer threads 
-    std::thread consumerThread2(consumer, 2);
-
-    providerThread.join(); ///< Waiting for provider thread to stop
-
-    consumerThread1.join(); ///< Waiting for consumer threads to stop
-    consumerThread2.join();
-
-    std::cout << "Execution completed\n";
-    return EXIT_SUCCESS; ///< We will always come here on ctrl + c
+    if (listen_fd != -1) 
+    {
+        close(listen_fd);
+    }
+    
+    if (client_fd != -1) 
+    {
+        close(client_fd);
+    }
+    return 0;
 }
